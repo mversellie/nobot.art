6a3c8a874650526679423133c10308ad90e0704f
marco <marco@versellie.com>
2024-06-07 21:23:11 -0400

    added liquibase

--- database/src/db_changelog/00001_ChangeLog.py
+++
@@ -1,8 +0,0 @@
-from database_layer import get_database
-#from database_objects import Example
-
-if __name__ == "__main__":
-    db = get_database()
-    db.connect()
-    #db.create_tables([Example])
-    db.close()
