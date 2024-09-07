ad5673bc4c1b7e13fc5d89cee49abe0679c5a404
marco <marco@versellie.com>
2024-09-07 16:48:09 -0400

    testing

--- keycloak/build-docker.sh
+++ keycloak/build-docker.sh
@@ -3,7 +3,7 @@ HOST="registry.digitalocean.com/nobot-art"
 VERSION=$1
 TAG_PREFIX="$HOST/keycloak"
 TAG_VERSION="$TAG_PREFIX:$VERSION"
-TAG_LATEST="$TAG_PREFIX:latest"
+TAG_LATEST="$TAG_PREFIX:latest1"
 docker build -t "$TAG_LATEST" -t "$TAG_VERSION" .
 docker push "$TAG_LATEST"
 docker push "$TAG_VERSION"
