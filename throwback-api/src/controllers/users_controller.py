72d5d7a95f3d5635d031092131f534bd694b8d07
marco <marco@versellie.com>
2024-09-09 17:17:55 -0400

    added additional security

--- throwback-api/src/controllers/users_controller.py
+++ throwback-api/src/controllers/users_controller.py
@@ -18,24 +18,21 @@ content_service = ContentService()
 settings_service = SettingsService()
 
 
-@users_controller.route('/users', methods=['POST'])
-def createUser():
+@users_controller.route('/users', methods=['POST','PUT'])
+def user_change():
     print("inside create user")
     user_request = request.get_json()
     auth_token = request.headers["Authorization"].split(" ")[1]
-    if auth_token != settings_service.get("KEYCLOAK_TO_NOBOT_API_ACCESS_TOKEN"):
-        print("keycloak authorization token set wrong in listener")
-        return handle_basic_error(403,"keycloak authorization token set wrong in listener")
+    if request.method == "POST":
+        if auth_token != settings_service.get("KEYCLOAK_TO_NOBOT_API_ACCESS_TOKEN"):
+            print("keycloak authorization token set wrong in listener")
+            return handle_basic_error(403,"keycloak authorization token set wrong in listener")
 
-    user_service.create_user(user_request["userId"],user_request["username"],user_request["email"])
-    #delete_new_user_private_messages(username)
-    return blank_ok()
-
-@users_controller.route('/users', methods=['PUT'])
-def update_user():
-    jwt_token = request.headers["Authorization"].split(" ")[1]
-    unlocked_token = keycloak_service.decode_jwt(jwt_token)
-    content_service.save_profile_pic(request.files.get("upload"),unlocked_token["preferred_username"])
+        user_service.create_user(user_request["userId"],user_request["username"],user_request["email"])
+        #delete_new_user_private_messages(username)
+    if request.method == "PUT":
+        unlocked_token = keycloak_service.decode_jwt(auth_token)
+        content_service.save_profile_pic(request.files.get("upload"),unlocked_token["preferred_username"])
     return blank_ok()
 
 def delete_new_user_private_messages(username:string):
